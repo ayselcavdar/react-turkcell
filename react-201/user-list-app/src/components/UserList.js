0adc934673f2711e7aa767125e52768ad232accf
Aysel Cavdar <a.misrikhanova13@gmail.com>
2022-11-28 14:31:08 +0300

    create user list by using axios

---
+++ react-201/user-list-app/src/components/UserList.js
@@ -0,0 +1,27 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+
+function UserList() {
+  const [users, setUsers] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    axios("https://jsonplaceholder.typicode.com/users")
+      .then((res) => setUsers(res.data))
+      .finally(() => setLoading(false));
+  }, []);
+  return (
+    <div>
+      <h2>Users</h2>
+      {loading && <div>Loading...</div>}
+
+      <ul className="user-list">
+        {users.map((user, id) => (
+          <li key={id}>{user.name}</li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default UserList;
