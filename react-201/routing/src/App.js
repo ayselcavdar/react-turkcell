a2dc64e2b343a65abd80b4f4e643d244bc17e5cf
Aysel Cavdar <a.misrikhanova13@gmail.com>
2022-12-08 18:04:34 +0300

    Layout-1

--- react-201/routing/src/App.js
+++ react-201/routing/src/App.js
@@ -3,18 +3,24 @@ import { Route, Routes, BrowserRouter } from "react-router-dom";
 import Home from "./pages/Home";
 import Users from "./pages/Users";
 import Contact from "./pages/Contact";
-import Menu from "./components/Menu";
 import UserDetail from "./pages/UserDetail";
+import DashboardLayout from "./layouts/DashboardLayout";
+import Login from "./pages/Login";
+import AuthLayout from "./layouts/AuthLayout";
 
 function App() {
   return (
     <BrowserRouter>
-      <Menu />
       <Routes>
-        <Route index element={<Home />} />
-        <Route path="users" element={<Users />} />
-        <Route path="users/:id" element={<UserDetail />} />
-        <Route path="contact" element={<Contact />} />
+        <Route path="/" element={<DashboardLayout />}>
+          <Route index element={<Home />} />
+          <Route path="users" element={<Users />} />
+          <Route path="users/:id" element={<UserDetail />} />
+          <Route path="contact" element={<Contact />} />
+        </Route>
+        <Route path="auth" element={<AuthLayout />}>
+          <Route path="login" element={<Login />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   );
