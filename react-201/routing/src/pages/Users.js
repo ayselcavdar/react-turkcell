b44152fdfbe0a0dd0125f5114de8301fdc9f6231
Aysel Cavdar <a.misrikhanova13@gmail.com>
2022-12-08 15:46:29 +0300

    working with useParams

--- react-201/routing/src/pages/Users.js
+++ react-201/routing/src/pages/Users.js
@@ -1,9 +1,24 @@
-import React from "react";
+import { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
 
 function Users() {
+  const [users, setUsers] = useState([]);
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((res) => res.json())
+      .then((data) => setUsers(data));
+  }, []);
+
   return (
     <div>
       <h2>Users</h2>
+      <ul>
+        {users.map((user) => (
+          <li key={user.id}>
+            <Link to={`${user.id}`}>{user.name}</Link>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
