44bc85ef455e57016e4aa6df85d7c88bf167f3a4
Aysel Cavdar <a.misrikhanova13@gmail.com>
2022-12-09 15:04:59 +0300

    working with formik

--- react-201/routing/src/pages/Dashboard/Contact.js
+++ react-201/routing/src/pages/Dashboard/Contact.js
@@ -1,9 +1,50 @@
 import React from "react";
+import { Formik } from "formik";
 
 function Contact() {
   return (
     <div>
       <h2>Contact</h2>
+      <Formik
+        initialValues={{
+          firstName: "",
+          lastName: "",
+          email: "",
+        }}
+        onSubmit={(values) => {
+          console.log(values);
+        }}
+      >
+        {({ handleSubmit, handleChange }) => (
+          <form onSubmit={handleSubmit}>
+            <label htmlFor="firstName">First Name</label>
+            <input
+              id="firstName"
+              name="firstName"
+              placeholder="Jane"
+              onChange={handleChange("firstName")}
+            />
+
+            <label htmlFor="lastName">Last Name</label>
+            <input
+              id="lastName"
+              name="lastName"
+              placeholder="Doe"
+              onChange={handleChange("lastName")}
+            />
+
+            <label htmlFor="email">Email</label>
+            <input
+              id="email"
+              name="email"
+              placeholder="jane@acme.com"
+              type="email"
+              onChange={handleChange("email")}
+            />
+            <button type="submit">Submit</button>
+          </form>
+        )}
+      </Formik>
     </div>
   );
 }
