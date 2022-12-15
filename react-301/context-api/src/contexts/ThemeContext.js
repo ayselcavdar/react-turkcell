d7aaa27ac8d9e062c13b5bbdeda9a0f3eec2ed50
Aysel Cavdar <a.misrikhanova13@gmail.com>
2022-12-15 17:22:45 +0300

    context provider isolation

--- react-301/context-api/src/contexts/ThemeContext.js
+++ react-301/context-api/src/contexts/ThemeContext.js
@@ -2,4 +2,10 @@ import { createContext } from "react";
 
 const ThemeContext = createContext();
 
+export const ThemeContextProvider = ({ children }) => {
+  return (
+    <ThemeContext.Provider value="lights">{children}</ThemeContext.Provider>
+  );
+};
+
 export default ThemeContext;
